63d2c6ff7473b47b8a6053eb637e0f2a2928f41b
Борис Стамов Милев <BSMilev18@codingburgas.bg>
2021-06-22 09:34:34 +0300

    Add project files.

---
+++ motel-exam-username/motel-exam-username.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Motel
+{
+private:
+    string name;
+    int capacity;
+    int currentBookingNumber;
+    int single = 50;
+    int doubl = 90;
+    int maisonette = 135;
+public:
+    int getRoompricing(string clientname, string roomtype, int nights, int rentnumber)
+    {
+        
+    }
+};
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
